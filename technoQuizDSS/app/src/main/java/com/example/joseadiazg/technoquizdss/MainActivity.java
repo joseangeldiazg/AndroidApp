8d8e5677ca8edcc3894a90796927d252dc00047c
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-01-10 20:38:09 +0100

    Añadidos botones a la priemera vista. avanza #2.

--- technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/MainActivity.java
+++ technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.joseadiazg.technoquizdss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "BOTON PULSADO", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
