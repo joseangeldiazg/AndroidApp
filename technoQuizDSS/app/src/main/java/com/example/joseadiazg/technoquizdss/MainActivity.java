292bcbf16837e9b69e7de7685d68c185e2934da5
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-01-25 15:33:59 +0100

    Relacionadas dos actividades. closes #4.

--- technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/MainActivity.java
+++ technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.joseadiazg.technoquizdss;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,21 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Obtenemos las vistas de los juegos que abriremos en el activity relacionado con los nuevos juegos
-
-        WebView juego1= (WebView) this.findViewById(R.id.webView1);
-        juego1.loadUrl("www.growsoundmag.com");
-
-        WebView juego2= (WebView) this.findViewById(R.id.webView2);
-        juego2.loadUrl("www.growsoundmag.com");
+        //Obtenemos las vistas de los juegos que abriremos en el activity relacionado con los nuevos juego
 
 
-        b1 = (Button) findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.button3);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "BOTON PULSADO", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(view.getContext(), MasJuegos.class);
+                startActivityForResult(intent, 0);
             }
         });
     }
---
+++ technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/MasJuegos.java
@@ -0,0 +1,28 @@
+package com.example.joseadiazg.technoquizdss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+/**
+ * Created by joseadiazg on 25/1/17.
+ */
+
+public class MasJuegos extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otrosjuegos);
+
+        WebView juego1= (WebView) this.findViewById(R.id.webView1);
+        juego1.loadUrl("http://growsoundmag.com/category/underground/");
+
+        WebView juego2= (WebView) this.findViewById(R.id.webView2);
+        juego2.loadUrl("http://growsoundmag.com/category/alternativo/");
+
+
+    }
+}
