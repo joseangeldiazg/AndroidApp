38111f1af830d6684037df7810513172df7645d6
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-02-01 20:03:42 +0100

    Instanciados los elementos de la actividad principal. closes #7

--- technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/ActivityJuego.java
+++ technoQuizDSS/app/src/main/java/com/example/joseadiazg/technoquizdss/ActivityJuego.java
@@ -2,7 +2,11 @@ package com.example.joseadiazg.technoquizdss;
 
 import android.app.Activity;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,13 +17,32 @@ import java.util.ArrayList;
 public class ActivityJuego extends Activity
 {
 
+
+    private final static int PREGUNTAS=2;
+
     private DBPref db;
 
     private Pregunta pregunta;
 
     private ArrayList<Pregunta>  listaPreguntas = new ArrayList<>();
 
-    private final static int PREGUNTAS=2;
+    private TextView preguntaView;
+
+    private Button respuesta1;
+    private Button respuesta2;
+    private Button respuesta3;
+    private Button respuesta4;
+
+    //Para las preguntas  de imagen y las de sonido deberemos crear un ImageView y MediaPlayer
+
+    private MediaPlayer preguntaSonido;
+    private ImageView preguntaImagen;
+
+    //Tendremos que declarar también lo necesario para el sonido de acierto o fallo
+
+    private MediaPlayer acierto;
+    private MediaPlayer fallo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,7 +50,21 @@ public class ActivityJuego extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preguntas);
 
-        db = new DBPref(this);
+        //Instanciamos los atributos
+
+        this.db = new DBPref(this);
+
+        this.preguntaView = (TextView) findViewById(R.id.preguntaView);
+
+        this.respuesta1 = (Button) findViewById(R.id.respuesta1);
+        this.respuesta2 = (Button) findViewById(R.id.respuesta2);
+        this.respuesta3 = (Button) findViewById(R.id.respuesta3);
+        this.respuesta4 = (Button) findViewById(R.id.respuesta4);
+
+        this.preguntaImagen = (ImageView) findViewById(R.id.preguntaImagen);
+
+        this.acierto = MediaPlayer.create(this, R.raw.acierto);
+        this.fallo = MediaPlayer.create(this, R.raw.fallo);
 
         //AQUI CUANDO CREAMOS ESTE CURSOR DEBEREMOS AÑADIR ALGUNA MANERA PARA ELEGIR LA CATEGORIA O LA DIFICULTAD -> OPCIONAL
 
@@ -56,4 +93,5 @@ public class ActivityJuego extends Activity
         }
         this.db.close();
     }
+
 }
